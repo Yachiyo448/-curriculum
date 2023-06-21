de561d723fd2943b00991f9aba96ddd46f2b9ad2
Yachiyo <Yachiyo448@gmail.com>
2023-06-21 22:30:36 +0900

    課題1.23更新

--- src/curriculum_1_23_question/AnimalInfo.java
+++ src/curriculum_1_23_question/AnimalInfo.java
@@ -1,62 +1,69 @@
 package curriculum_1_23_question;
 
 public class AnimalInfo {
-	
-	// フィールド
-	private String name = "";	// 名前
-	private double height = 0.0d;	// 体長
-	private int speed = 0;	// 速度
-	
+
+	// フィールドを宣言
+	// 名前
+	private String name;
+	// 体長(m)
+	private double height;
+	// 速度(km/h)
+	private int speed;
+
+	// コンストラクタ
+	public AnimalInfo() {
+	}
+
 	// setterメソッド
 
 	/**
-	 * 動物の名前を設定します。
+	 * 動物の名前を設定
 	 * @param animalName 動物名
 	 */
 	public void setName(String animalName) {
 		this.name = animalName;
 	}
-	
+
 	/**
-	 * 動物の体長(m)を設定します。
+	 * 動物の体長(m)を設定
 	 * @param animalHeight 動物の体長
 	 */
 	public void setHeight(double animalHeight) {
 		this.height = animalHeight;
 	}
-	
+
 	/**
-	 * 動物の速度(km/h)を設定します。
+	 * 動物の速度(km/h)を設定
 	 * @param animalSpeed 動物の速度
 	 */
 	public void setSpeed(int animalSpeed) {
 		this.speed = animalSpeed;
 	}
-	
+
 	// getterメソッド
-	
+
 	/**
-	 * 動物の名前を取得します。
+	 * 動物の名前を取得
 	 * @return 動物の名前
 	 */
 	public String getName() {
-		return this.name;
-	}
-	
-	/**
-	 * 動物の体長(m)を取得します。
-	 * @return 動物の体長
-	 */
-	public double getHeight() {
-		return this.height;
+		return name;
 	}
 
 	/**
-	 * 動物の速度(km/h)を取得します。
+	 * 動物の体長(m)を取得
+	 * @return 動物の体長
+	 */
+	public double getHeight() {
+		return height;
+	}
+
+	/**
+	 * 動物の速度(km/h)を取得
 	 * @return 動物の速度
 	 */
 	public int getSpeed() {
-		return this.speed;
+		return speed;
 	}
 
 }
